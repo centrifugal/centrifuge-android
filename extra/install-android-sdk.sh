6f4550bd05f088749f71537cb0d28e10650c69e7
Semyon <samvimes@yandex.ru>
2017-01-07 21:51:10 +0300

    q

--- extra/install-android-sdk.sh
+++ extra/install-android-sdk.sh
@@ -9,7 +9,7 @@ export PATH="$PATH:${ANDROID_HOME}/tools"
 export DEBIAN_FRONTEND=noninteractive
 
 sudo apt-get -qq update && \
-    apt-get install -qqy --no-install-recommends \
+    sudo apt-get install -qqy --no-install-recommends \
       curl \
       html2text \
       openjdk-8-jdk \
@@ -19,16 +19,16 @@ sudo apt-get -qq update && \
       lib32ncurses5 \
       lib32z1 \
       unzip \
-    && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+    && sudo rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
-rm -f /etc/ssl/certs/java/cacerts; \
+sudo rm -f /etc/ssl/certs/java/cacerts; \
     /var/lib/dpkg/info/ca-certificates-java.postinst configure
 
 curl -s http://dl.google.com/android/repository/tools_r${VERSION_SDK_TOOLS}-linux.zip > /tools.zip && \
     unzip /tools.zip -d /sdk && \
     rm -v /tools.zip
 
-mkdir -p $ANDROID_HOME/licenses/ \
+sudo mkdir -p $ANDROID_HOME/licenses/ \
   && echo "8933bad161af4178b1185d1a37fbf41ea5269c55" > $ANDROID_HOME/licenses/android-sdk-license \
   && echo "84831b9409646a918e30573bab4c9c91346d8abd" > $ANDROID_HOME/licenses/android-sdk-preview-license
 
